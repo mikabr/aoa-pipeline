99b5e3edebdd5d3c2dfa685091132c44cd89e522
Eva Portelance <portelance.eva@gmail.com>
2021-08-11 16:00:19 -0700

    added vif calculation by language and measure

--- scripts/collinearity.R
+++ scripts/collinearity.R
@@ -1,7 +1,20 @@
 
-get_vif_collinearity_scores <- function(df, dependent_variable, predictors){
-  formula <- glue("{dependent_variable} ~ {paste(predictors, collapse = ' + ')}")  %>% as.formula()
+get_vif_collinearity_scores <- function(df, predictors){
+  formula <- glue("aoa ~ {paste(predictors, collapse = ' + ')}")  %>% as.formula()
   model <- lm(formula, data=df)
   vif_results = car::vif(model)
-  return(vif_results)
+  df.vif <- as.data.frame(vif_results)
+  df.vif <- tibble::rownames_to_column(df.vif, "predictors")
+
+  return(df.vif)
+}
+
+get_vif_bylang_bymeasure<- function(df, predictors){
+  grouped_vif <- df |>
+    group_by(language, measure) |>
+    group_nest() |>
+    mutate(vif_results = map(.x = data, .f =~get_vif_collinearity_scores(.x, predictors))) |>
+    unnest(vif_results)
+
+  return(grouped_vif)
 }
