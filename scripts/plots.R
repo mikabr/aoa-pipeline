003ac836f9bdf6ef521e0d6c0fb268e8346c048d
georgialoukatou <georgialoukatou@gmail.com>
2021-09-21 19:07:15 -0700

    add plot by lex category abstract category

--- scripts/plots.R
+++ scripts/plots.R
@@ -56,7 +56,7 @@ ggplot(cv_across_lang, aes(x = mean_abs_dev, y = language)) +
 aoa_mad_lex_cat<- function(cv_across_lex, lang, meas, wb_data, preds ){
 
   cv_across <- cv_across_lex %>%
-    filter(pr %in% preds ) %>%
+   # filter(pred_ %in% preds ) %>%
     group_by(lexical_category) %>%
     summarise(mad=n()) %>%
     mutate(mad=mad/sum(mad))
@@ -95,10 +95,10 @@ pdf(file = "data/reliability_plot.pdf",
 
 
 relia_plot <- function(dfinal, lang_list){
- ggplot(dfinal %>% filter(language %in% lang_list), aes(y = predictor, x=r, fill=predictor)) +
+ ggplot(dfinal %>% filter(language %in% lang_list), aes(y = predictor, x=rsquared, fill=preds)) +
   geom_bar(position="dodge", stat="identity") +
   facet_grid(rows = vars(measure), cols=vars(language)) +
-  geom_errorbar(position=position_dodge(width=0.9), aes(y=predictor, x=threshold_half, xmax=threshold_half, xmin=threshold_half,  color=predictor)) +
+  geom_errorbar(position=position_dodge(width=0.9), aes(y=preds, x=threshold_half, xmax=threshold_half, xmin=threshold_half,  color=predictor)) +
   theme(legend.position = "bottom") +
   ylab("Predictor") +
   xlab("R2") +
@@ -108,5 +108,18 @@ relia_plot <- function(dfinal, lang_list){
 }
 dev.off()
 
+coefs_by_pred_all<- function(all_coefs){
+  all_coefs_1 <- all_coefs %>%
+    group_by(measure, term, interact_lexcat) %>%
+    summarise(mean=mean(estimate))
+  all_coefs <- all_coefs   |>
+  left_join(all_coefs_1)
 
-
+  ggplot(all_coefs, aes(x = estimate, y = term, colour = language)) +
+    facet_grid(interact_lexcat~measure, scales = "free")+
+    geom_point(aes(shape = signif), size = 3, alpha = 0.8) +
+    geom_point(data=all_coefs_1,  mapping=aes(x = mean, y = term), col="black",size = 3, shape = 3, fill="black")+
+    labs(x = "Coefficient estimate", y = "Predictors") +
+#    xlim(-4,4) +
+    theme_bw()
+}
