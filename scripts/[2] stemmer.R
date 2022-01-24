d9b365ebe65da8d7d13cb5aaf1888b6b747eb5b5
alv1nn <alvin.tan1996@gmail.com>
2022-01-24 15:40:58 -0800

    updated stemming

--- scripts/[2] stemmer.R
+++ scripts/[2] stemmer.R
@@ -1,31 +1,29 @@
 
 # Gets stems for a list of words in a given language.
-# Uses Snowball by default
-# Also allows for hunspell as an alternative method
+# Uses Snowball by default, no stemming for Chinese languages, and hunspell otherwise
 
 stem <- function(words, language) {
-  if (language != "Croatian" & language!= "Mandarin (Beijing)" & language!= "Mandarin (Taiwanese)"){
-  lang_snowball <- convert_lang_stemmer(language, "snowball")
-  lang_hunspell <- convert_lang_stemmer(language, "hunspell")
-  if (lang_snowball %in% SnowballC::getStemLanguages()) {
-    SnowballC::wordStem(words, lang_snowball)
-  } else if (lang_hunspell %in% hunspell::list_dictionaries()) {
-    map_chr(words, \(word) {
-      stem <- hunspell::hunspell_stem(word, dictionary(lang_hunspell))[[1]]
-      return(if (length(stem) == 0) word else stem[1])
-    })
-  }} else if (language == "Croatian"){
-    chunk_size <- 1000
-    word_chunks <- split(words, ceiling(seq_along(words) / chunk_size))
-    map(word_chunks, function(word_chunk) {
-      system2("python",
-              args = c("scripts/croatian_stemmer/croatian.py", sprintf('"%s"', word_chunk)),
-              stdout = TRUE)
-    }) |> unlist()}
-else if (language == "Mandarin (Beijing)"){
+  if (language %in% c("Mandarin (Beijing)", "Mandarin (Taiwanese)", "Cantonese")) {
+    # no stemming
     return(words)
-}
-else if (language == "Mandarin (Taiwanese)"){
-    return(words)
-}
+  } else if (language %in% c("Hebrew", "Korean", "Croatian", "Czech")) {
+    # hunspell
+    lang <- convert_lang_stemmer(language, "hunspell")
+    if (lang %in% hunspell::list_dictionaries()) {
+      map_chr(words, \(word) {
+        stem <- hunspell::hunspell_stem(word, hunspell::dictionary(lang))[[1]]
+        return(if (length(stem) == 0) word else stem[1])
+      })
+    } else {
+      message(glue("Hunspell dictionary for {language} not installed"))
+    }
+  } else {
+    # snowball
+    lang <- convert_lang_stemmer(language, "snowball")
+    if (lang %in% SnowballC::getStemLanguages()) {
+      SnowballC::wordStem(words, lang)
+    } else {
+      message(glue("Language {language} has no Snowball stemmer"))
+    }
+  }
 }
